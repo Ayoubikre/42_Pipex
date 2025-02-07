cd4c4d9da97a498601b8760e62849476fc019a83
Ayoub Akritah <aakritah@c1r8p3.1337.ma>
2025-02-07 18:45:22 +0100

    add

---
+++ a.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   a.c                                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 13:22:49 by aakritah          #+#    #+#             */
+/*   Updated: 2025/02/07 18:42:32 by aakritah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "a.h"
+
+int	main(void)
+{
+	printf("hello -> len= %lu\n", strlen("hello"));
+	printf("world -> len= %lu\n", ft_strlen("wrold"));
+	// ft_printf("world -> len= %d\n", ft_strlen("world"));
+}
