582a9c9214a9ef50b5fdc6f7bd86fcacf2f618ef
Ayoub Akritah <aakritah@c2r9p5.1337.ma>
2025-02-12 14:02:56 +0100

    Add

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: noctis <noctis@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 18:46:07 by aakritah          #+#    #+#             */
-/*   Updated: 2025/02/12 01:17:32 by noctis           ###   ########.fr       */
+/*   Updated: 2025/02/12 13:57:38 by aakritah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,20 @@ void	ft_exit(char *t)
 	perror(t);
 	exit(1);
 }
-char	*ft_fix_path(char *cmd, char **env)
+void	ft_free(char **t)
+{
+	int	i;
+
+	i = 0;
+	while (t[i])
+	{
+		free(t[i]);
+		i++;
+	}
+	free(t);
+}
+
+char	**ft_fix_path(char **env)
 {
 	char	**t;
 	char	*tmp;
@@ -39,49 +52,68 @@ char	*ft_fix_path(char *cmd, char **env)
 	tmp = ft_strdup((const char *)env[i]);
 	t = ft_split((const char *)tmp + 5, ':');
 	if (!t)
-		exit(1);
-	i = 0;
-	free(tmp);
-	while (t[i])
-	{
-		tmp = ft_strjoin(t[i], "/");
-		tmp = ft_strjoin(tmp, cmd);
-		if (!tmp)
-			exit(0);
-		if (access(tmp, R_OK) != -1)
-		{
-			j = 1;
-			break ;
-		}
-	}
-	if (j == 0)
-		exit(0);
-	return (tmp);
+		ft_exit("failes path split");
+	return (free(tmp), t);
 }
 
-void	ft_process(int c, char **ar, char **env, int pip_arr[2])
+void	ft_execve(char *ar, char **env)
 {
-	int		fd1;
+	int		i;
+	char	*pathname;
+	char	*tmp;
 	char	**cmd;
-	char	*path;
+	char	**path;
 
-	fd1 = open(ar[1], O_RDONLY);
-	if (fd1 == -1)
-		ft_exit("failed fd");
-	dup2(fd1, STDIN_FILENO);
-	dup2(pip_arr[1], STDOUT_FILENO);
-	close(fd1);
-	close(pip_arr[1]);
-	cmd = ft_split(ar[3], ' ');
+	cmd = ft_split(ar, ' ');
 	if (!cmd)
-		exit(0);
-	path = ft_fix_path(cmd[0], env);
-	if (!path)
-		exit(0);
-	
-	execve(path, cmd, env);
-	ft_printf("\n -- done2 -- \n");
-	ft_exit("failed execve");
+		ft_exit("failes cmd split");
+	path = ft_fix_path(env);
+	i = 0;
+	while (path[i])
+	{
+		tmp = ft_strjoin(path[i], "/");
+		pathname = ft_strjoin(tmp, cmd[0]);
+		free(tmp);
+		if (access(pathname, R_OK) != -1)
+			break ;
+		free(pathname);
+		i++;
+	}
+	if (!path[i])
+		ft_exit("failes to found cmd path");
+	execve(pathname, cmd, env);
+	ft_exit("failed axecve");
+}
+
+int	ft_file_check(char *t, int f)
+{
+	int	fd;
+
+	if (access(t, R_OK) == -1)
+		ft_exit("failed file not found");
+	if (f == 1)
+	{
+		fd = open(t, O_RDONLY | O_CLOEXEC);
+		if (fd == -1)
+			ft_exit("failed fd");
+		return (fd);
+	}
+	fd = open(t, O_WRONLY | O_CLOEXEC);
+	if (fd == -1)
+		ft_exit("failed fd");
+	return (fd);
+}
+
+void	ft_process(char **ar, char **env, int pip_arr[2])
+{
+	int	fd;
+
+	fd = ft_file_check(ar[1], 1);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	// dup2(pip_arr[1], STDOUT_FILENO);
+	// close(pip_arr[1]);
+	ft_execve(ar[2], env);
 }
 
 int	main(int c, char **ar, char **env)
@@ -91,19 +123,19 @@ int	main(int c, char **ar, char **env)
 	int		pip_arr[2];
 
 	// int		i=0;
-	// atexit(leaks);
+	atexit(leaks);
 	if (c != 3)
-		ft_exit("failed");
+		ft_exit("failed argv count ");
 	else
 	{
-		if (pipe(pip_arr) == -1)
-			ft_exit("failed pipe");
+		// if (pipe(pip_arr) == -1)
+		// 	ft_exit("failed pipe");
 		pid1 = fork();
 		if (pid1 < 0)
 			ft_exit("failed fork");
 		if (pid1 == 0)
 		{
-			ft_process(c, ar, env, pip_arr);
+			ft_process(ar, env, pip_arr);
 		}
 		// pid2 = fork();
 		// if (pid2 < 0)
@@ -112,6 +144,7 @@ int	main(int c, char **ar, char **env)
 		// {
 		// 	path = ft_fix_path(env);
 		// }
+		waitpid(pid1, NULL, WNOHANG);
 		printf("\n -- done -- \n");
 	}
 }
